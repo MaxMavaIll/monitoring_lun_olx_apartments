4983ee82aa96db78d9fcb337420a05d5dc9ac3ba
Black1Matter <blackmatter19.70@gmail.com>
2024-10-18 13:04:22 +0300

    add try except

--- Lun/monitoring.py
+++ Lun/monitoring.py
@@ -21,62 +21,64 @@ log.addHandler(log_s)
 
 
 def check_flat_with_filter(token, chain_id, data):
-    
-    if "LUN" not in data:
-        data["LUN"] = []
+    try:
+        if "LUN" not in data:
+            data["LUN"] = []
 
-    response = requests.get(BASE_URL)
-    if response.status_code == 200:
+        response = requests.get(BASE_URL)
+        if response.status_code == 200:
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        apartments = soup.find_all('article')  # Знайти всі блоки з оголошеннями
+            soup = BeautifulSoup(response.content, 'html.parser')
+            apartments = soup.find_all('article')  # Знайти всі блоки з оголошеннями
 
-        log.info(f"Len apartments {len(apartments)}")
-        for index, apartment in enumerate(apartments):
-            # Отримуємо id з атрибута id тега <article>
-            article_id = apartment.get('id')
-            
-            if article_id:
-                full_link = URL_LINK + article_id  # Створюємо повне посилання
-            else:
-                full_link = "Посилання немає"
+            log.info(f"Len apartments {len(apartments)}")
+            for index, apartment in enumerate(apartments):
+                # Отримуємо id з атрибута id тега <article>
+                article_id = apartment.get('id')
+                
+                if article_id:
+                    full_link = URL_LINK + article_id  # Створюємо повне посилання
+                else:
+                    full_link = "Посилання немає"
 
-            # Пошук тексту опису квартири
-            description = apartment.find('p').get_text(strip=True) if apartment.find('p') else "Опису немає"
+                # Пошук тексту опису квартири
+                description = apartment.find('p').get_text(strip=True) if apartment.find('p') else "Опису немає"
 
-            # Пошук параметра за правильним CSS-селектором (використовуємо :nth-child)
-            param_element1 = apartment.select_one('button > div > div > div > div:nth-child(1) > span')
-            rooms = param_element1.get_text(strip=True) if param_element1 else "Параметра немає"
+                # Пошук параметра за правильним CSS-селектором (використовуємо :nth-child)
+                param_element1 = apartment.select_one('button > div > div > div > div:nth-child(1) > span')
+                rooms = param_element1.get_text(strip=True) if param_element1 else "Параметра немає"
 
-            param_element2 = apartment.select_one('button > div > div > div > div:nth-child(2) > span')
-            areas = param_element2.get_text(strip=True) if param_element2 else "Параметра немає"
+                param_element2 = apartment.select_one('button > div > div > div > div:nth-child(2) > span')
+                areas = param_element2.get_text(strip=True) if param_element2 else "Параметра немає"
 
-            param_element3 = apartment.select_one('button > div > div > div > div:nth-child(3) > span')
-            floor = param_element3.get_text(strip=True) if param_element3 else "Параметра немає"
+                param_element3 = apartment.select_one('button > div > div > div > div:nth-child(3) > span')
+                floor = param_element3.get_text(strip=True) if param_element3 else "Параметра немає"
 
-            price_element = apartment.select_one('button > div > div:nth-child(1) > div')
-            price = price_element.get_text(strip=True) if price_element else "Ціни немає"
+                price_element = apartment.select_one('button > div > div:nth-child(1) > div')
+                price = price_element.get_text(strip=True) if price_element else "Ціни немає"
 
-            street_element = apartment.select_one('article > div > div > h3 > button')
-            street = street_element.get_text(strip=True) if street_element else "Ціни немає"
-            
-            message = f"""
-<strong>LUN</strong>
+                street_element = apartment.select_one('article > div > div > h3 > button')
+                street = street_element.get_text(strip=True) if street_element else "Ціни немає"
+                
+                message = f"""
+    <strong>LUN</strong>
 
-Ціна: {price}
-Вулиця: <code>{street}</code> 
-Опис: 
-<b>{description}</b>
+    Ціна: {price}
+    Вулиця: <code>{street}</code> 
+    Опис: 
+    <b>{description}</b>
 
-Кімнати: {rooms}
-Площа: {areas}
-Поверх: {floor}
-<a href="{full_link}">Перейти на сайт</a>
-"""
-            
-            if article_id not in data["LUN"]:
-                send_message(message, token, chain_id)
-                data["LUN"].append(article_id)
+    Кімнати: {rooms}
+    Площа: {areas}
+    Поверх: {floor}
+    <a href="{full_link}">Перейти на сайт</a>
+    """
+                
+                if article_id not in data["LUN"]:
+                    send_message(message, token, chain_id)
+                    data["LUN"].append(article_id)
 
-    else:
-        log.error("LUN | Помилка при доступі до сторінки:", response.status_code)
+        else:
+            log.error("LUN | Помилка при доступі до сторінки:", response.status_code)
+    except Exception as e:
+        log.error(e)
--- OLX/monitoring.py
+++ OLX/monitoring.py
@@ -1,5 +1,5 @@
 import requests
-import logging as log
+import logging
 import time
 
 from bs4 import BeautifulSoup
@@ -14,54 +14,73 @@ areas_filter = 'Загальна площа: '
 areas_kitchen_filter = 'Загальна кухні: '
 room_filter = 'Кількість кімнат: '
 
+
+
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+log_s = logging.StreamHandler()
+log_s.setLevel(logging.INFO)
+formatter2 = logging.Formatter(
+    "%(name)s %(asctime)s %(levelname)s %(message)s")
+log_s.setFormatter(formatter2)
+log.addHandler(log_s)
+
 def check_flat_with_filter(token, chain_id, data):
+    try:
     
-    if "OLX" not in data:
-        data["OLX"] = []
+        if "OLX" not in data:
+            data["OLX"] = []
 
-    response = requests.get(BASE_URL)
-    if response.status_code == 200:
+        response = requests.get(BASE_URL)
+        if response.status_code == 200:
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-        apartments = soup.find_all("div", class_="css-1sw7q4x")  
+            soup = BeautifulSoup(response.content, 'html.parser')
+            apartments = soup.find_all("div", class_="css-1sw7q4x")  
 
+            log.info(f"Len apartments {len(apartments)}")
+            for index, apartment in enumerate(apartments):
+                article_id = apartment.get('id')
 
-        for index, apartment in enumerate(apartments):
-            article_id = apartment.get('id')
+                if article_id not in data["OLX"]:
 
-            if article_id not in data["OLX"]:
-
-                link = apartment.find('a') if apartment.find('a') else "Опису немає"
-                
-                full_link = URL_LINK + link['href'] if article_id else None
-
-                if full_link is None:
-                    continue
-
-                response = requests.get(full_link)
-                if response.status_code == 200:
-
-                    soup = BeautifulSoup(response.content, 'html.parser')
-                    info_about_apartment = soup.find_all('li', class_="css-1r0si1e")
-
-                    for index, info in enumerate(info_about_apartment):
-                        
-                        text = info.find('p').get_text(strip=True) if info.find('p') else "Опису немає"
-
-                        floor = text[len(floor_filter):] if floor_filter in text else "Опису немає"
-
-                        areas = text[len(areas_filter):] if areas_filter in text else "-"
-
-                        areas_kit = text[len(areas_kitchen_filter):] if areas_kitchen_filter in text else "-"
-
-                        rooms = text[len(room_filter):] if room_filter in text else "Опису немає"
-                        
-                    description = soup.find('div', class_="css-1o924a9").get_text(strip=True) if soup.find('div', class_="css-1o924a9") else "Опису немає"
-
-                    price = soup.find('h3', class_='css-90xrc0').get_text(strip=True) if soup.find('h3', class_='css-90xrc0') else "Опису немає"
-                    street = soup.find('h4', class_='css-1kc83jo').get_text(strip=True) if soup.find('h4', class_='css-1kc83jo') else "Опису немає"
+                    link = apartment.find('a') if apartment.find('a') else "Опису немає"
                     
-                    message = f"""
+                    full_link = URL_LINK + link['href'] if article_id else None
+
+                    if full_link is None:
+                        continue
+
+                    response = requests.get(full_link)
+                    if response.status_code == 200:
+
+                        soup = BeautifulSoup(response.content, 'html.parser')
+                        info_about_apartment = soup.find_all('li', class_="css-1r0si1e")
+                        text = "Опису немає"
+                        floor = "Опису немає"
+                        areas = "-"
+                        areas_kit = "-"
+                        rooms = "Опису немає"
+
+                        for index, info in enumerate(info_about_apartment):
+                            
+                            text = info.find('p').get_text(strip=True) if info.find('p') else "Опису немає"
+
+                            floor = text[len(floor_filter):] if floor_filter in text else "Опису немає"
+
+                            areas = text[len(areas_filter):] if areas_filter in text else "-"
+
+                            areas_kit = text[len(areas_kitchen_filter):] if areas_kitchen_filter in text else "-"
+
+                            rooms = text[len(room_filter):] if room_filter in text else "Опису немає"
+                            
+                        description = soup.find('div', class_="css-1o924a9").get_text(strip=True) if soup.find('div', class_="css-1o924a9") else "Опису немає"
+
+                        price = soup.find('h3', class_='css-90xrc0').get_text(strip=True) if soup.find('h3', class_='css-90xrc0') else "Опису немає"
+                        street = soup.find('h4', class_='css-1kc83jo').get_text(strip=True) if soup.find('h4', class_='css-1kc83jo') else "Опису немає"
+                        
+                        message = f"""
     <strong>OLX</strong>
                     
     Ціна: {price}
@@ -73,13 +92,15 @@ def check_flat_with_filter(token, chain_id, data):
     Площа: {areas}/-/{areas_kit}
     Поверх: {floor}
         <a href="{full_link}">Перейти на сайт</a>
-    """
-                    
-                    send_message(message, token, chain_id)
-                    data["OLX"].append(article_id)
-                    time.sleep(5)
+        """
+                        
+                        send_message(message, token, chain_id)
+                        data["OLX"].append(article_id)
+                        time.sleep(5)
 
-    else:
-        log.error("LUN | Помилка при доступі до сторінки:", response.status_code)
+        else:
+            log.error("LUN | Помилка при доступі до сторінки:", response.status_code)
+    except Exception as e:
+        log.error(e)
 
 # check_flat_with_filter()
